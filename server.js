7a5317f3e8f7753266191169f54215b4655ceb9d
sid-who <46945966+sid-who@users.noreply.github.com>
2019-12-09 18:58:04 -0800

    Server.js update for default heroku port

--- server.js
+++ server.js
@@ -22,3 +22,9 @@ let port = process.env.PORT || 4000;
 //   });
 // });
 app.use(express.static(__dirname + "/final-twocan"));
+
+// default Heroku PORT
+// app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
